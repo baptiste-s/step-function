c96d6e65f4c6e20353a9051685eb7d9dcc3bbda7
baptiste-s <38555475+baptiste-s@users.noreply.github.com>
2025-10-24 09:42:19 +0200

    Add logging to UniqueIDTagGroup methodstoto

--- plugin.go
+++ plugin.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
+	"log"
 	"os"
 	"github.com/bridgecrewio/yor/src/common/structure"
 	"github.com/bridgecrewio/yor/src/common/tagging"
@@ -15,16 +16,19 @@ type UniqueIDTagGroup struct {
 }
 
 func (d *UniqueIDTagGroup) CreateTagsForBlock(block structure.IBlock) error {
+	log.Println("[PLUGIN] CreateTagsForBlock appelé pour:", block.GetResourceID())
 	return d.UpdateBlockTags(block, block)
 }
 
 func (d *UniqueIDTagGroup) GetDefaultTags() []tags.ITag {
+	log.Println("[PLUGIN] GetDefaultTags appelé")
 	return []tags.ITag{
 		&UniqueIDTag{},
 	}
 }
 
 func (d *UniqueIDTagGroup) InitTagGroup(_ string, skippedTags []string, explicitlySpecifiedTags []string, options ...tagging.InitTagGroupOption) {
+	log.Println("[PLUGIN] InitTagGroup appelé")
 	opt := tagging.InitTagGroupOptions{
 		TagPrefix: "",
 	}
@@ -41,36 +45,34 @@ type UniqueIDTag struct {
 }
 
 func (t *UniqueIDTag) Init() {
+	log.Println("[PLUGIN] UniqueIDTag.Init appelé")
 	t.Key = "carma-name"
 }
 
 func (t *UniqueIDTag) CalculateValue(data interface{}) (tags.ITag, error) {
+	log.Println("[PLUGIN] CalculateValue appelé")
 	block, ok := data.(structure.IBlock)
 	if !ok {
 		return nil, fmt.Errorf("failed to convert data to IBlock")
 	}
 
-	// Lire env et team depuis les variables d'environnement
 	env := os.Getenv("YOR_ENV")
 	if env == "" {
 		env = "unknown"
 	}
-
 	team := os.Getenv("YOR_TEAM")
 	if team == "" {
 		team = "unknown"
 	}
 
 	resourceID := block.GetResourceID()
-
-	// Calculer le MD5
 	hash := md5.Sum([]byte(resourceID))
 	hashString := hex.EncodeToString(hash[:])
-
-	// Construire la valeur finale (TOUT est calculé, pas d'expression Terraform)
 	value := fmt.Sprintf("%s-%s-%s", env, team, hashString)
-
+	
+	log.Printf("[PLUGIN] Tag généré: %s = %s\n", t.Key, value)
 	return &tags.Tag{Key: t.Key, Value: value}, nil
 }
 
-var ExtraTagGroups = []interface{}{&UniqueIDTagGroup{}}
+// IMPORTANT : Essayez cette export
+var TagGroup tagging.ITagGroup = &UniqueIDTagGroup{}
